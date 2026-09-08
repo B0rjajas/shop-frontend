9b8adc1a0d3d63622691d83d8c244426a2195849
B0rjajas <xabierlaita@gmail.com>
2026-09-08 21:44:50 +0200

    shop-admin new folders

---
+++ shop-admin/src/routes/index.ts
@@ -0,0 +1,7 @@
+import LoginView from '@/views/LoginView.vue';
+
+// En las rutas:
+{
+  path: '/login',
+  component: LoginView,
+}
---
+++ shop-admin/src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<template>
+    <Login :isAdminMode="true" />
+  </template>
+  
+  <script setup>
+  import Login from '@/components/Login.vue';
+  </script>
